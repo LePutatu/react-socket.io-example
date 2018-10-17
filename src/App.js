2b82710319536fa0368815ddf7291c9b68ffaa91
LePutatu <nikolastkanev@gmail.com>
2018-10-17 17:07:46 +0300

    setting up starting example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer } from './api';
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  subscribeToTimer((err, timestamp) => this.setState({
+    timestamp
+  }));
+}
+state = {
+  timestamp: 'no timestamp yet'
+};
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p className="App-intro">
+      This is the timer value: {this.state.timestamp}
+      </p>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
