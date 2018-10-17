646da3456379fb755776faebc0e12a39f78c35d9
LePutatu <nikolastkanev@gmail.com>
2018-10-17 20:18:42 +0300

    testing sedning json

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const io = require('socket.io')();
 
 io.on('connection', (client) => {
   client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
+    console.log('client is subscribing to timer with interval ', interval.localInterval);
+    console.log(interval);
     setInterval(() => {
       client.emit('timer', new Date());
-    }, interval);
+    }, interval.localInterval);
   });
+  client.on("disconnect", () => console.log("Client disconnected"));
 });
 
 const port = 8000;
--- src/api.js
+++ src/api.js
@@ -3,6 +3,13 @@ const  socket = openSocket('http://localhost:8000');
 
 function subscribeToTimer(cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
-  socket.emit('subscribeToTimer', 1000);
+  socket.emit('subscribeToTimer', {
+    someJSON:{
+      value1:1,
+      value2:2,
+      value3:4
+    },
+    localInterval:1000
+  });
 }
 export { subscribeToTimer };
